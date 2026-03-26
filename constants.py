4ecf8e1c344f4d4e9015e345d72eacbb2dd747e8
GitCloneHenry <hwagner1015@gmail.com>
2026-03-26 13:16:59 -0600

    Add SOTMConstants class and update shooter RPM table comments; implement ShotCalculator class

--- constants.py
+++ constants.py
@@ -55,6 +55,7 @@ class ShooterConstants:
 
     minimum_acceptable_closed_loop_error: float = 1.0
 
+    # Values calculated using 5962's ProjectileSimulator (https://github.com/eeveemara/frc-fire-control)
     rpm_table: Dict[float, float] = {
         0.55: 6000,
         0.60: 3750,
@@ -264,3 +265,40 @@ class FieldConstants:
             else (FieldConstants.blue_hub_pose.X(), FieldConstants.blue_hub_pose.Y())
         )
         return hub_x, hub_y
+
+
+class SOTMConstants:
+    launcher_offset_x: float = 0.2
+    launcher_offset_y: float = 0.0
+
+    min_scoring_distance: float = 0.5
+    max_scoring_distance: float = 5.0
+
+    max_iterations: int = 25
+    convergance_tolerance: float = 0.001
+    tof_min: float = 0.05
+    tof_max: float = 5.0
+
+    min_sotm_speed: float = 0.1
+
+    max_sotm_speed: float = 3.0
+
+    phase_delay_ms: float = 30.0
+    mech_latency_ms: float = 20.0
+
+    sotm_drag_coeff: float = 0.24
+
+    convergence: float = 1.0
+    velocity_stability: float = 0.8
+    vision_confidence: float = 1.2
+    heading_accuracy: float = 1.5
+    distance_in_range: float = 0.5
+    heading_max_error_rad = 15 * pi / 180
+
+    heading_speed_scalar: float = 1.0
+
+    heading_reference_distance: float = 2.5
+
+    max_tilt_deg: float = 5.0
+
+    shooter_angle_offset_rad: float = 0.0
---
+++ utility/shot_calculator.py
@@ -0,0 +1,17 @@
+# 5962's Shot Calculator (https://github.com/eeveemara/frc-fire-control) ported to python.
+
+from wpimath.geometry import Pose2d, Rotation2d, Translation2d, Twist2d
+from wpimath.kinematics import ChassisSpeeds
+from wpimath.interpolation
+
+# Launch Parameters
+# (rpm: float, time_of_flight_sec: float, drive_angle: Rotation2d, drive_angular_velocity_rad_per_sec: float, is_valid: bool, confidence: float, solved_distance_m: float, iterations_used: int, warm_start_used: bool):
+
+# ShotInputs
+# (robot_pose: Pose2d, field_velocity: ChassisSpeeds, hub_center: Translation2d, hub_forward: Translation2d, vision_confidence: float, pitch_deg: float, roll_deg: float)
+
+# TODO: implement version of wpilib's InterpolatingDoubleTreeMap
+
+
+class ShotCalculator:
+    pass
